278df0b5b37def9e7fe4d809e7fdd1b4e5086c24
binary-code-978 <93479009+binary-code-978@users.noreply.github.com>
2022-03-11 15:41:11 -0500

    Add files via upload

--- kwitter_room.js
+++ kwitter_room.js
@@ -33,7 +33,7 @@ getData();
 function addRoom() {
       room_name = document.getElementById("room_name").value;
 
-      firebase.database().ref("/").child(room_name).update({
+      firebase.database().ref("/").child(room.name).update({
             purpose: "adding room name"
       });
 
